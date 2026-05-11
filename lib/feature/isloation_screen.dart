810e25a116d6ca7265fb687c62aa458e4c3717d8
mdharis090 <harisnazeer090@gmail.com>
2026-05-11 15:09:17 +0500

    islotion concept with used

--- lib/feature/isloation_screen.dart
+++ lib/feature/isloation_screen.dart
@@ -1,94 +1,4 @@
 
-// import 'dart:async';
-// import 'package:flutter/material.dart';
-
-// class TweenAnimationScreen extends StatefulWidget {
-//   const TweenAnimationScreen({super.key});
-
-//   @override
-//   State<TweenAnimationScreen> createState() => _TweenAnimationScreenState();
-// }
-
-// class _TweenAnimationScreenState extends State<TweenAnimationScreen>
-//     with SingleTickerProviderStateMixin {
-//   late AnimationController _controller;
-//   late Animation<double> _animation;
-//   late Timer timer;
-
-//   double currentSize = 50;
-
-//   @override
-//   void initState() {
-//     super.initState();
-
-//     _controller = AnimationController(
-//       vsync: this,
-//       duration: const Duration(seconds: 1),
-//     );
-
-//     _animation = Tween<double>(begin: 50, end: 400).animate(
-//       CurvedAnimation(parent: _controller, curve: Curves.easeInOut),
-//     )
-//       ..addListener(() {
-//         setState(() {
-//           currentSize = _animation.value;
-//         });
-//       });
-
-//     //  Auto animation using Timer
-//     timer = Timer.periodic(const Duration(seconds: 2), (timer) {
-//       if (_controller.isCompleted) {
-//         _controller.reverse();
-//       } else {
-//         _controller.forward();
-//       }
-//     });
-//   }
-
-//   //  Heavy loop (freeze UI)
-//   void heavyLoop() {
-//     int sum = 0;
-//     for (int i = 0; i < 1000000000; i++) {
-//       sum += i;
-//     }
-//     print("Done: $sum");
-//   }
-
-//   @override
-//   void dispose() {
-//     timer.cancel();
-//     _controller.dispose();
-//     super.dispose();
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(title: const Text("Auto Tween Freeze UI ")),
-
-//       body: Padding(
-//         padding: const EdgeInsets.all(8.0),
-//         child: Center(
-//           child: Container(
-//             height: currentSize,
-//             width: currentSize,
-//             decoration: BoxDecoration(
-//               color: Colors.blue,
-//               borderRadius: BorderRadius.circular(20),
-//             ),
-//           ),
-//         ),
-//       ),
-
-//       // 🟢 FAB → trigger heavy loop (freeze)
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: heavyLoop,
-//         child: Text('Get'),
-//      //   child: const Icon(Icons.warning),
-//       ),
-//     );
-//   }
-// }
 import 'dart:async';
 import 'dart:isolate';
 import 'package:flutter/material.dart';
@@ -172,7 +82,6 @@ class _TweenAnimationScreenState extends State<TweenAnimationScreen>
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Isolate Animation Demo")),
-
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
